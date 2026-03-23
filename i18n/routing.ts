d40c8f652d4b360bdf16dc1f24e458f6a9ff1731
MaxZhang <zsc.guru@gmail.com>
2026-03-23 18:43:02 +0800

    refactor: 国际化语言默认为中文，同时设置根据Accept-Language header 自动切换

--- i18n/routing.ts
+++ i18n/routing.ts
@@ -1,21 +1,17 @@
-import {defineRouting} from 'next-intl/routing';
-import {createNavigation} from 'next-intl/navigation';
- 
-export const routing = defineRouting({
-  // A list of all locales that are supported
-  locales: ['en', 'zh'],
- 
-  // Used when no locale matches
-  defaultLocale: 'en',
-  
-  // Don't use a prefix for the default locale
-  localePrefix: 'as-needed',
+import { createNavigation } from 'next-intl/navigation'
+import { defineRouting } from 'next-intl/routing'
 
-  // Disable automatic locale detection based on browser headers
-  localeDetection: false
-});
- 
+export const routing = defineRouting({
+	// A list of all locales that are supported
+	locales: ['en', 'zh'],
+
+	// Used when no locale matches
+	defaultLocale: 'zh',
+
+	// Don't use a prefix for the default locale
+	localePrefix: 'as-needed',
+})
+
 // Lightweight wrappers around Next.js' navigation APIs
 // that will consider the routing configuration
-export const {Link, redirect, usePathname, useRouter, getPathname} =
-  createNavigation(routing);
+export const { Link, redirect, usePathname, useRouter, getPathname } = createNavigation(routing)
